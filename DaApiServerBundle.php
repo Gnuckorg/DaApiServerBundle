2132049a46d8455d9d56930cc4ef7425bd824ea4
Thomas <thomas.prelot@tessi.fr>
2013-07-29 17:25:01 +0200

    First commit

---
+++ DaApiServerBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Da\ApiServerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DaApiServerBundle extends Bundle
+{
+}
---
+++ Resources/doc/index.md
@@ -0,0 +1,4 @@
+Documentation of DaApiServerBundle
+==============================
+
+This is the documentation of the bundle DaApiServerBundle.
